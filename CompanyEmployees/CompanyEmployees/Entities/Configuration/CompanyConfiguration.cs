c4bbe6859a129e1c2c9de7ddd8677e4e721faf28
Thush <uthushanthini@gmail.com>
2022-06-10 02:12:10 +0530

    Initial commit

---
+++ CompanyEmployees/CompanyEmployees/Entities/Configuration/CompanyConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CompanyEmployees.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyEmployees.Entities.Configuration
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            
+        }
+    }
+}
---
+++ CompanyEmployees/CompanyEmployees/Entities/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CompanyEmployees.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyEmployees.Entities.Configuration
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            
+        }
+    }
+}
